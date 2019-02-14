7a87a9ce8dbf8a85b2680cc63ffeeaac86cd48e2
KirGorod <kgorodisy@gmail.com>
2019-02-15 01:56:50 +0200

    commit message

--- src/pages/views.py
+++ src/pages/views.py
@@ -14,7 +14,8 @@ def contact_view(request, *args,**kwargs):
 def about_view(request, *args,**kwargs):
     my_context = {
         "my_text": "This is about us",
+        "this_is_true": True,
         "my_number": 123,
-        "my_list": [123, 42342, 324234]
+        "my_list": [312, 42342, 324234, "Abc"]
     }
     return render(request, "about.html", my_context)
